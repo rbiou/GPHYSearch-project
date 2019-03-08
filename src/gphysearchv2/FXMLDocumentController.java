604275a749d4da3c8eb0f84397859bc3e8170900
remib <remi.biou@gmail.com>
2019-03-08 15:52:21 +0100

    functionnal project bby

--- src/gphysearchv2/FXMLDocumentController.java
+++ src/gphysearchv2/FXMLDocumentController.java
@@ -5,32 +5,27 @@
  */
 package gphysearchv2;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import static javafx.scene.input.KeyCode.I;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  *
@@ -38,87 +33,96 @@ import javafx.stage.Stage;
  */
 public class FXMLDocumentController implements Initializable {
     
-    
+    private Integer index;
+    private ObservableList<Etudiant> etudiants = FXCollections.observableArrayList();
     @FXML
-    private Label title_label;
+    private Label title_label, error1, error2;
     @FXML
     private ImageView title_icon;
     @FXML
-    private AnchorPane add_pane, home_pane;
+    private AnchorPane add_pane, home_pane, edit_pane;
     @FXML
-    private TextField name_field, surname_field, year_field;
+    private TextField name_field, surname_field, year_field, edit_name_field, edit_surname_field, edit_year_field;
     @FXML
-    private MenuButton promotion_field;
-    @FXML
-    private MenuItem promotion_1, promotion_2, promotion_3;
-    @FXML
-    private Button addButton;
+    private MenuButton promotion_field, edit_promotion_field;
     @FXML
     private TableView<Etudiant> personTable;
     @FXML
-    private TableColumn<Etudiant, String> prenomColonne,nomColonne,anneeDeNaissanceColonne,promotionColonne;
-  
+    private TableColumn<Etudiant, String> prenomColonne, nomColonne, anneeDeNaissanceColonne, promotionColonne;
     @FXML
-    private Label label,error1;    
-    @FXML
-    private Label prenomLabel; 
-    @FXML
-    private Label nomLabel;   
-    @FXML
-    private Label promotionLabel;   
-    @FXML
-    private Label anneeDeNaissanceLabel;
-  
-    
-   
-    
+    private TableColumn<Etudiant, Void> editerColonne;
+
     @FXML
     private void setSelectedPromotion(ActionEvent event) {
-        promotion_field.setText(((MenuItem)event.getTarget()).getText());
+        if (add_pane.isVisible()) {
+            promotion_field.setText(((MenuItem) event.getTarget()).getText());
+        } else {
+            edit_promotion_field.setText(((MenuItem) event.getTarget()).getText()); 
+        }
     }
-    
+
     @FXML
     private void setAddPanel(MouseEvent event) {
         title_label.setText("Ajouter un étudiant");
         title_icon.setImage(new Image(getClass().getResource("baseline_person_add_white_48dp.png").toExternalForm()));
         add_pane.setVisible(true);
         home_pane.setVisible(false);
+        edit_pane.setVisible(false);
     }
-    
+
     @FXML
     private void setHomePanel(MouseEvent event) {
         title_label.setText("Accueil");
         title_icon.setImage(new Image(getClass().getResource("baseline_home_white_48dp.png").toExternalForm()));
         add_pane.setVisible(false);
         home_pane.setVisible(true);
+        edit_pane.setVisible(false);
     }
-    
+
     @FXML
     private void addStudent(ActionEvent event) {
-        
         Etudiant newStudent = new Etudiant(surname_field.getText(), name_field.getText(), promotion_field.getText(), year_field.getText());
-        //allPromotions.addEtudiant(newStudent);
-        //Set home panel back
-        if ((name_field.getText().isEmpty()==false) && (surname_field.getText().isEmpty()==false) && (promotion_field.getText().isEmpty()==false) && (year_field.getText().isEmpty()==false) ) {
-                    System.out.println("coucou");
-
-        
-        title_label.setText("Accueil");
-        title_icon.setImage(new Image(getClass().getResource("baseline_home_white_48dp.png").toExternalForm()));
-        add_pane.setVisible(false);
-        home_pane.setVisible(true);
-        //System.out.println(allPromotions.size());
-        error1.setVisible(false);
-        personTable.getItems().add(newStudent);
-        }
-        else{
+        if ((name_field.getText().isEmpty() == false) && (surname_field.getText().isEmpty() == false) && (promotion_field.getText().isEmpty() == false) && (year_field.getText().isEmpty() == false)) {
+            //Set home panel back            
+            title_label.setText("Accueil");
+            title_icon.setImage(new Image(getClass().getResource("baseline_home_white_48dp.png").toExternalForm()));
+            add_pane.setVisible(false);
+            home_pane.setVisible(true);
+            //If any empty field
+            error1.setVisible(false);
+            //Add the new student to the student lists 
+            personTable.getItems().add(newStudent);
+            name_field.clear();
+            surname_field.clear();
+            year_field.clear();
+        } else {
             error1.setVisible(true);
         }
     }
     
+    @FXML
+    private void editStudent(ActionEvent event) {
+        Etudiant editedStudent = new Etudiant(edit_surname_field.getText(), edit_name_field.getText(), edit_promotion_field.getText(), edit_year_field.getText());
+        if ((edit_name_field.getText().isEmpty() == false) && (edit_surname_field.getText().isEmpty() == false) && (edit_promotion_field.getText().isEmpty() == false) && (edit_year_field.getText().isEmpty() == false)) {
+            //Edit student
+            etudiants.set(index, editedStudent);
+            //Set home panel back            
+            title_label.setText("Accueil");
+            title_icon.setImage(new Image(getClass().getResource("baseline_home_white_48dp.png").toExternalForm()));
+            edit_pane.setVisible(false);
+            home_pane.setVisible(true);
+            //If any empty field
+            error2.setVisible(false);
+            //Clear all fields for next edit
+            edit_name_field.clear();
+            edit_surname_field.clear();
+            edit_year_field.clear();
+        } else {
+            error2.setVisible(true);
+        }
+    }
 
-
-      @Override
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
         nomColonne.setCellValueFactory(new PropertyValueFactory<>("nom"));
         prenomColonne.setCellValueFactory(new PropertyValueFactory<>("prenom"));
@@ -126,17 +130,47 @@ public class FXMLDocumentController implements Initializable {
         promotionColonne.setCellValueFactory(new PropertyValueFactory<>("promotion"));
         personTable.setItems(getPeople());
         personTable.setEditable(true);
+
+        //Set Edit button column
+        TableColumn<Etudiant, Void> editerColonne = new TableColumn("Editer");
+        Callback<TableColumn<Etudiant, Void>, TableCell<Etudiant, Void>> cellFactory = (final TableColumn<Etudiant, Void> param) -> {
+            final TableCell<Etudiant, Void> cell = new TableCell<Etudiant, Void>() {
+                private final Button btn = new Button("Editer");
+                {
+                    btn.setOnAction((ActionEvent event) -> {
+                        title_label.setText("Editer un étudiant");
+                        Etudiant etudiant = getTableView().getItems().get(getIndex());
+                        home_pane.setVisible(false);
+                        add_pane.setVisible(false);
+                        edit_pane.setVisible(true);
+                        edit_name_field.setText(etudiant.getNom());
+                        edit_surname_field.setText(etudiant.getPrenom());
+                        edit_year_field.setText(etudiant.getAnneeDeNaissance());
+                        edit_promotion_field.setText(etudiant.getPromotion());
+                        index = getIndex();
+                    });
+                }
+
+                @Override
+                public void updateItem(Void item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        setGraphic(btn);
+                    }
+                }
+            };
+            return cell;
+        };
+        editerColonne.setCellFactory(cellFactory);
+        personTable.getColumns().add(editerColonne);
     }
-    
-    public ObservableList<Etudiant>  getPeople()
-    {
-        ObservableList<Etudiant> etudiants = FXCollections.observableArrayList();
-        etudiants.add(new Etudiant("Frank","Sinatra","L3", "1999"));
-        etudiants.add(new Etudiant("Rebecca","Fergusson","M1", "1999"));
-        etudiants.add(new Etudiant("Mr.","T","M2", "1999"));
-       
-        
+
+    public ObservableList<Etudiant> getPeople() {
+//        etudiants.add(new Etudiant("Frank", "Sinatra", "L3", "1999"));
+//        etudiants.add(new Etudiant("Rebecca", "Fergusson", "M1", "1999"));
+//        etudiants.add(new Etudiant("Mr.", "T", "M2", "1999"));
         return etudiants;
     }
 }
-
